5daba47bf976a6f1e67154c3ade2fd2c7e3cc35f
Christoph <christoph_rohde@icloud.com>
2023-12-19 20:07:58 +0100

    Add LogStream.hpp

---
+++ EnvironmentLog/LogStream.hpp
@@ -0,0 +1,131 @@
+//
+// Created by Christoph Rohde on 17.12.23.
+//
+
+#pragma once
+
+#include "LogLevel.hpp"
+
+#include <iostream>
+
+class LogStream {
+private:
+    bool colorize;
+    LogLevel displayedLogLevel;
+    std::ostream &stream;
+
+
+    LogStream(bool colorize, LogLevel displayedLogLevel, std::ostream &stream = std::cout):
+            colorize(colorize),
+            displayedLogLevel(displayedLogLevel),
+            stream(stream)  {}
+
+    /// LogLevel filter
+    inline auto logLevelFilter(LogLevel logLevel) -> bool {
+        return logLevel >= displayedLogLevel;
+    }
+
+    /// Colorize
+
+
+public:
+
+    /// Singleton
+    static auto create(const bool colorize, const LogLevel displayedLogLevel, std::ostream &stream = std::cout) -> LogStream& {
+        static LogStream logStream = LogStream(colorize, displayedLogLevel, stream);
+        return logStream;
+    }
+
+    template<typename T>
+    auto operator<<(T const &t) -> LogStream & {
+        stream << t;
+        return *this;
+    }
+
+    auto operator<<(std::ostream &(*f)(std::ostream &)) -> LogStream & {
+        stream << f;
+        return *this;
+    }
+
+    auto operator<<(std::ios &(*f)(std::ios &)) -> LogStream & {
+        stream << f;
+        return *this;
+    }
+
+    auto operator<<(std::ios_base &(*f)(std::ios_base &)) -> LogStream & {
+        stream << f;
+        return *this;
+    }
+
+    auto operator<<(std::string_view s) -> LogStream & {
+        stream << s;
+        return *this;
+    }
+
+    auto operator<<(std::string const &s) -> LogStream & {
+        stream << s;
+        return *this;
+    }
+
+    auto operator<<(char const *s) -> LogStream & {
+        stream << s;
+        return *this;
+    }
+
+    auto operator<<(char c) -> LogStream & {
+        stream << c;
+        return *this;
+    }
+
+    auto operator<<(int i) -> LogStream & {
+        stream << i;
+        return *this;
+    }
+
+    auto operator<<(unsigned int i) -> LogStream & {
+        stream << i;
+        return *this;
+    }
+
+    auto operator<<(long i) -> LogStream & {
+        stream << i;
+        return *this;
+    }
+
+    auto operator<<(unsigned long i) -> LogStream & {
+        stream << i;
+        return *this;
+    }
+
+    auto operator<<(long long i) -> LogStream & {
+        stream << i;
+        return *this;
+    }
+
+    auto operator<<(unsigned long long i) -> LogStream & {
+        stream << i;
+        return *this;
+    }
+
+    auto operator<<(float f) -> LogStream & {
+        stream << f;
+        return *this;
+    }
+
+    auto operator<<(double f) -> LogStream & {
+        stream << f;
+        return *this;
+    }
+
+    auto operator<<(long double f) -> LogStream & {
+        stream << f;
+        return *this;
+    }
+
+    auto operator<<(LogLevel const logLevel) -> LogStream & {
+//        if (logLevelFilter(logLevel)) {
+            stream << logLevel;
+//        }
+        return *this;
+    }
+};

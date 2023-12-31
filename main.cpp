82a6aece02954c904f387c695d49304200051849
Christoph <christoph_rohde@icloud.com>
2023-12-31 15:05:22 +0100

    Test Loglevel Filter

--- main.cpp
+++ main.cpp
@@ -16,7 +16,16 @@ int main() {
     std::filesystem::path logPath = std::filesystem::current_path() / "logs";
     std::cout << "Log to: " << logPath << std::endl;
 
-    auto log = EnvironmentLog();
+    auto log = EnvironmentLog({
+        .logDirectory = logPath,
+        .dateFormat = "%d. %b %Y",
+        .timeFormat = "%T",
+        .logLevel = LogLevel::ERROR,
+        .colorize = true
+    });
+
+
+
     log.print("Hello ${0} ${1}",
               {"World", "!"},
               std::source_location::current());
@@ -33,14 +42,33 @@ int main() {
 
 
 
-    LogStream logStream2 = LogStream::create(true, LogLevel::INFO);
-    logStream2 << LogLevel::ERROR << " test error" << std::endl;
-    logStream2 << LogLevel::INFO << " test info" << std::endl;
+    LogStream logStream2 = LogStream::create();
+    logStream2 << std::endl;
+    logStream2 << std::endl;
 
-    LogStream logStream3 = LogStream::create(true, LogLevel::ERROR);
-    logStream3 << LogLevel::ERROR << " test error" << std::endl;
-    logStream3 << LogLevel::INFO << " test info" << std::endl;
 
+
+    LogEntry testEntry = {
+            .logLevel = LogLevel::ERROR,
+            .message = "test error message"
+    };
+
+    LogEntry testEntry2 = {
+            .logLevel = LogLevel::INFO,
+            .message = "test info message"
+    };
+//            .sourceLocation = std::source_location::current()
+//    };
+
+    logStream2 << "Test ausgabe 1: " << testEntry << std::endl;
+    logStream2 << "Test ausgabe 2: " << testEntry2 << std::endl;
+
+
+    LogStream logStream3 = LogStream::create();
+    logStream3 << "Test ausgabe 3: " << testEntry << std::endl;
+    logStream3 << "Test ausgabe 4: " << testEntry2 << std::endl;
+
+    // Colorized for individual log or MutltiStream
 
 
     return 0;
